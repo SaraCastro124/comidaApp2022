2a2c3bf9888e202b78bc3189ab72458d24a5b64a
SaraCastro124 <sara1330castro@gmail.com>
2022-05-26 14:56:44 -0300

    ultimocambio

--- bd/dump.sql
+++ bd/dump.sql
@@ -5,4 +5,4 @@ CALL altaRestaurante (5, 'havanna@gmail.com', 'havanna', '9 de julio', 'stord_96
 CALL altaRestaurante (3, 'tucson@gmail.com', 'tucson', 'Av. Cordoba', 'mine_.763');
 CALL altaPlato ('albondipapa', 'albondigas con papas fritas', 1, 300, 3, 50);
 CALL altaPlato ('frappuchino', 'cafe con espuma y leche', 4, 480, 5, 169);
-CALL altaPlato ('papas fritas', 'papas fritas', 2, 300, 3, 50);
+CALL altaPlato ('papas fritas', 'papas fritas', 2, 300, 3, 50);  
