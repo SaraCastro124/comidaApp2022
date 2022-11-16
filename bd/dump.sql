4438308e893ff55fe416607c704f900bc1a076ef
abrilchauq <abril.chauqueet12d1@gmail.com>
2022-11-16 16:45:37 -0300

    dump

--- bd/dump.sql
+++ bd/dump.sql
@@ -9,3 +9,4 @@ CALL altaPlato ('albondipapa', 'albondigas con papas fritas', @idAlbondipapa, 30
 CALL altaPlato ('frappuchino', 'cafe con espuma y leche', @idfrappuccino, 480, @idHavanna, 169);
 CALL altaPlato ('papas fritas', 'papas fritas', @idpapas, 300, @idTucson, 50);  
 CALL registrarCliente (@idAbril, 'Abril', 'Chauque','ab@gmail.com', 's3');
+CALL altaDetallePedido (@idPlato, 55, 1, 300);
